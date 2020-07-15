9efc2f13e68a09adbd1d4e5c33a45187af87fee9
Симатов Алексей Юрьевич <Aleksei_Simatov@epam.com>
2020-07-15 04:47:53 +0400

    Тесты для группы аккордеонов

---
+++ src/components/organisms/AccordionGroup/AccordionGroup.test.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+import 'jest-styled-components';
+import { AccordionGroup } from './AccordionGroup';
+import { Paragraph } from '../../atoms/Paragraph';
+
+describe('AccordionGroup', () => {
+  it('AccordionGroup should render correctly by default', () => {
+    const accordionGroup = renderer
+      .create(
+        <AccordionGroup
+          content={[
+            { id: '1', title: 'accordion 1', description: 'description 1' },
+            { id: '2', title: 'accordion 2', description: 'description 2' },
+          ]}
+        />,
+      )
+      .toJSON();
+    expect(accordionGroup).toMatchSnapshot();
+  });
+
+  it('AccordionGroup should render correctly when initiallyExpanded=1', () => {
+    const accordionGroup = renderer
+      .create(
+        <AccordionGroup
+          content={[
+            { id: '1', title: 'accordion 1', description: 'description 1' },
+            { id: '2', title: 'accordion 2', description: 'description 2' },
+          ]}
+          initiallyExpanded={1}
+          DescriptionComponent={Paragraph}
+        />,
+      )
+      .toJSON();
+    expect(accordionGroup).toMatchSnapshot();
+  });
+
+  it('AccordionGroup should render correctly when forceExpanded=1', () => {
+    const accordionGroup = renderer
+      .create(
+        <AccordionGroup
+          content={[
+            { id: '1', title: 'accordion 1', description: 'description 1' },
+            { id: '2', title: 'accordion 2', description: 'description 2' },
+          ]}
+          forceExpanded={1}
+          DescriptionComponent={Paragraph}
+        />,
+      )
+      .toJSON();
+    expect(accordionGroup).toMatchSnapshot();
+  });
+
+  it('Accordion should expand and collapse on click', () => {
+    const accordionGroup = mount(
+      <AccordionGroup
+        content={[
+          { id: '1', title: 'accordion 1', description: 'description 1' },
+          { id: '2', title: 'accordion 2', description: 'description 2' },
+        ]}
+        DescriptionComponent={Paragraph}
+      />,
+    );
+
+    const accordionTitle = accordionGroup.find('TitleContainer').first();
+
+    accordionTitle.simulate('click');
+
+    expect(accordionGroup.find('DescriptionContainer')).toHaveLength(1);
+
+    accordionTitle.simulate('click');
+
+    expect(accordionGroup.find('DescriptionContainer')).toHaveLength(0);
+  });
+});
